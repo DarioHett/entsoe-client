a193137270782e73909c1e53000364f76342c224
Dario Hett <40806448+DarioHett@users.noreply.github.com>
2024-01-04 17:52:17 +0100

    Revert "Added the 'Outages' (Excluded 10.1.C) parser, bumped version of pytz."

--- tests/test_outages.py
+++ tests/test_outages.py
@@ -1,8 +1,8 @@
 import logging
 import unittest
-import os
 
 from pandas import DataFrame
+from settings import *
 
 from entsoe_client import Client
 from entsoe_client.ParameterTypes import *
@@ -15,11 +15,11 @@ class IntegrationTest(unittest.TestCase):
     def setUpClass(cls) -> None:
         cls.queries = [
             # All unvailable.
-            Outages.UnavailabilityOfConsumptionUnits(
-                    biddingZone_Domain=Area('FR'),
-                    periodStart=202108240000,
-                    periodEnd=202108250000
-            ),
+            # Outages.UnavailabilityOfConsumptionUnits(
+            #         biddingZone_Domain=Area('FR'),
+            #         periodStart=202108240000,
+            #         periodEnd = 202108250000
+            # ),
             Outages.UnavailabilityOfGenerationUnits(
                 biddingZone_Domain=Area("FR"),
                 periodStart=202108240000,
@@ -30,11 +30,11 @@ class IntegrationTest(unittest.TestCase):
                 periodStart=202108240000,
                 periodEnd=202108250000,
             ),
-            # Outages.UnavailabilityOfOffshoreGridInfrastructure(
-            #     biddingZone_Domain=Area("DE_TENNET"),
-            #     periodStart=202108010000,
-            #     periodEnd=202108250000,
-            # ),
+            Outages.UnavailabilityOfOffshoreGridInfrastructure(
+                biddingZone_Domain=Area("DE_TENNET"),
+                periodStart=202108010000,
+                periodEnd=202108250000,
+            ),
             Outages.UnavailabilityOfTransmissionInfrastructure(
                 in_Domain=Area("DE_50HZ"),
                 out_Domain=Area("PL_CZ"),
@@ -44,7 +44,7 @@ class IntegrationTest(unittest.TestCase):
         ]
 
     def test_integration(self):
-        client = Client(api_key=os.environ["API_KEY"])
+        client = Client(api_key=api_key)
         self.assertIsInstance(client, Client)
 
         query = self.queries[0]
@@ -53,16 +53,22 @@ class IntegrationTest(unittest.TestCase):
         response = client.download(query)
         self.assertTrue(response.ok)
 
-        df = Parser.parse(response)
-        self.assertIsInstance(df, DataFrame)
+        # df = Parser.parse(response)
+        # self.assertIsInstance(df, DataFrame)
+        with self.assertRaises(NotImplementedError) as context:
+            Parser.parse(response)
 
     def test_all(self):
-        client = Client(api_key=os.environ["API_KEY"])
+        client = Client(api_key=api_key)
         for query in self.queries:
             with self.subTest(type(query).__name__):
+                logging.debug(type(query).__name__)
+                print(type(query).__name__)
                 response = client.download(query)
-                df = Parser.parse(response)
-                self.assertIsInstance(df, DataFrame)
+                # df = Parser.parse(response)
+                # self.assertIsInstance(df, DataFrame)
+                with self.assertRaises(NotImplementedError) as context:
+                    Parser.parse(response)
 
 
 if __name__ == "__main__":
